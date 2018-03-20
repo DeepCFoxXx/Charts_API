1e5922eae168bad84f6f991ff6bd6855030b1640
DeepCFoxXx <learncode23@gmail.com>
2018-03-20 12:44:09 +0000

    added onload function

---
+++ public/app.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function() {
+  var data = new ChartData();
+  var containers = new ChartContainers();
+  var types = new ChartTypes();
+  var title = "Pokemon";
+  new Chart(title,types.pie, containers.pie,data.pie);
+  new Chart(title,types.line,containers.line,data.series, data.months);
+  new Chart(title,types.column,containers.column,data.series, data.months);
+});
